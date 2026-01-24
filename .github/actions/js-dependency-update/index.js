db426b07501f0d2f34bc79532bd23779a8562bed
Prudhvi Yashwanth Reddy Kikkuru <prudhviyashwanthme@gmail.com>
2026-01-24 10:44:06 +0530

    New custom Action Js Based

--- .github/actions/js-dependency-update/index.js
+++ .github/actions/js-dependency-update/index.js
@@ -1,6 +1,10 @@
 const core = require('@actions/core');
  
 async function run() { 
+  /*
+  1. Parse inputs: Base Branch from which to check for updates
+  2. Target-branch to use to create the PR
+  */
   core.info('I am a custom JS action');
 }
  
